c7bd66c32454b785dd123dd8f6ea9070dea9ba0f
Justin Demetroff <43005128+jdemetroff-LiveData@users.noreply.github.com>
2024-01-28 16:22:10 -0500

    Fix formatting and remove unused code

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app = flask.Flask(__name__)
 @app.route('/')
 def index():
     name, title = random.choice(list(summons.summons.items()))
-
     return flask.render_template('index.html', name=name, title=title)
 
 
--- ffxivapi_comm.py
+++ ffxivapi_comm.py
@@ -2,34 +2,35 @@ import asyncio
 import os
 import logging
 import aiohttp
-import pyxivapi
-from pyxivapi.models import Filter, Sort
-
-if os.environ.get("ffxivapi_key"):
-    api_key = os.environ.get("ffxivapi_key")
-else:
-    try:
-        import keyring
-        api_key = keyring.keyring.ffxivapi_key
-    except ImportError:
-        print("No API key found. Please set the environment variable 'ffxivapi_key' to your API key.")
-        exit()
 
 
-async def fetch_example_results():
-    client = pyxivapi.XIVAPIClient(api_key=api_key)
+class FfxivApiComm:
+    def __init__(self):
+        # look for a key in the environment variables or pull from keyring.py
+        if os.environ.get("ffxivapi_key"):
+            self.api_key = os.environ.get("ffxivapi_key")
+        else:
+            try:
+                import keyring
+                self.api_key = keyring.keyring.ffxivapi_key
+            except ImportError:
+                print("No API key found. Please set the environment variable 'ffxivapi_key' to your API key.")
+                exit()
 
-    # Search ingame data for matches against a given query. Includes item, minion, mount & achievement descriptions, quest dialog & more.
-    lore = await client.lore_search(
-        query="Summons",
-        language="en"
-    )
+    async def request(self, url: str, columns: list = None):
+        headers = {
+            'User-Agent': '&lt;User-Agent&gt;',
+            'columns': ','.join(columns) if columns else None,
+            'private_key': self.api_key
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=headers) as response:
+                return await response.json()
 
-    print(lore)
-
-    await client.session.close()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='%H:%M')
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(fetch_example_results())
+    ffxivapicomm = FfxivApiComm()
+    loop = asyncio.new_event_loop()
+    data = loop.run_until_complete(ffxivapicomm.request(url='http://xivapi.com/Item/1675', columns=['Name']))
+    print(data)
